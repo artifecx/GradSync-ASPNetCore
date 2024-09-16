a1f89cff21915f4dbc503e570c953812e16dfca5
artifecx <kebeniga@gmail.com>
2024-09-16 23:06:49 +0800

    Add initial Education system

---
+++ Data/Interfaces/IEducationRepository.cs
@@ -0,0 +1,22 @@
+﻿using Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    public interface IEducationRepository
+    {
+        Task<List<College>> GetAllCollegesAsync();
+        Task<List<Department>> GetAllDepartmentsAsync();
+        Task<List<YearLevel>> GetAllYearLevelsAsync();
+        Task AddCollegeAsync(College college);
+        Task AddDepartmentAsync(Department department);
+        Task UpdateCollegeAsync(College college);
+        Task UpdateDepartmentAsync(Department department);
+        Task DeleteCollegeAsync(College college);
+        Task DeleteDepartmentAsync(Department department);
+    }
+}
---
+++ Services/Interfaces/IEducationService.cs
@@ -0,0 +1,12 @@
+﻿using Data.Models;
+using Services.ServiceModels;
+using System.Threading.Tasks;
+using static Resources.Constants.Enums;
+
+namespace Services.Interfaces
+{
+    public interface IEducationService
+    {
+        Task<EducationViewModel> GetAllAsync();
+    }
+}
