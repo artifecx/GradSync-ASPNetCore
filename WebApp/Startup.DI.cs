56dbb12649170c3f78dff0ed03de5ca829a57827
artifecx <kebeniga@gmail.com>
2024-09-17 11:59:53 +0800

    Remove Education system codes

--- WebApp/Startup.DI.cs
+++ WebApp/Startup.DI.cs
@@ -36,12 +36,10 @@ namespace WebApp
             this._services.TryAddSingleton<TokenValidationParametersFactory>();
             this._services.AddScoped<IAccountService, AccountService>();
             this._services.AddScoped<IUserService, UserService>();
-            this._services.AddScoped<IEducationService, EducationService>();
 
             // Repositories
             this._services.AddScoped<IUserRepository, UserRepository>();
             this._services.AddScoped<IAdminRepository, AdminRepository>();
-            this._services.AddScoped<IEducationRepository, EducationRepository>();
 
             // Manager Class
             this._services.AddScoped<SignInManager>();
