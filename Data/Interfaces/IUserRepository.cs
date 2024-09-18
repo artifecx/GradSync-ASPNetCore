e285ad69034497dfd9ea0cb117d8fbc55e0f56c5
artifecx <kebeniga@gmail.com>
2024-09-18 23:39:31 +0800

    Modify User system - Implement proper behavior when changing roles - Add checks for no changes made when editing

--- Data/Interfaces/IUserRepository.cs
+++ Data/Interfaces/IUserRepository.cs
@@ -14,10 +14,12 @@ namespace Data.Interfaces
         void AddApplicant(Applicant applicant);
         void AddRecruiter(Recruiter recruiter);
         void AddAdmin(Admin nlo);
-        Task UpdateAsync(User user);
-        Task DeleteAsync(string UserId);
-        Task<bool> UserExistsAsync(string UserId);
-        Task<User> FindByIdAsync(string id);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(string userId);
+        Task DeleteAdminAsync(string adminId);
+        Task DeleteApplicantAsync(string applicantId);
+        Task DeleteRecruiterAsync(string recruiterId);
+        Task<User> FindUserByIdAsync(string id);
         Task<Role> FindRoleByIdAsync(string id);
         Task<List<Role>> GetAllRolesAsync();
     }
--- Services/Interfaces/IAccountService.cs
+++ Services/Interfaces/IAccountService.cs
@@ -10,5 +10,8 @@ namespace Services.Interfaces
         LoginResult AuthenticateUser(string email, string password, ref User user);
         void RegisterUser(AccountServiceModel model);
         bool UserExists(string Email);
+        void AddAdmin(User user);
+        void AddApplicant(User user);
+        void AddRecruiter(User user);
     }
 }
