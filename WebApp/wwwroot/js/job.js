238998450e1490ff7d884d04b5712fdd0c1c4c2c
artifecx <kebeniga@gmail.com>
2024-09-25 07:23:32 +0800

    Add Job detail view, Recruiter all jobs view

---
+++ WebApp/wwwroot/js/job.js
@@ -0,0 +1,66 @@
+﻿$(document).ready(function () {
+    var url = new URL(window.location);
+    var showModal = url.searchParams.get('showModal');
+
+    if (showModal === 'editJob') {
+        $('#editJobModal').modal('show');
+        url.searchParams.delete('showModal');
+        window.history.replaceState(null, null, url);
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var contentTextarea = document.getElementById('createJobTitle');
+    var remainingCharsSpan = document.getElementById('remainingTitleChars');
+
+    function updateRemainingChars() {
+        var remaining = 100 - contentTextarea.value.length;
+        remainingCharsSpan.textContent = remaining + ' characters remaining';
+    }
+
+    updateRemainingChars();
+
+    contentTextarea.addEventListener('keyup', updateRemainingChars);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var contentTextarea = document.getElementById('createJobDescription');
+    var remainingCharsSpan = document.getElementById('remainingDescriptionChars');
+
+    function updateRemainingChars() {
+        var remaining = 800 - contentTextarea.value.length;
+        remainingCharsSpan.textContent = remaining + ' characters remaining';
+    }
+
+    updateRemainingChars();
+
+    contentTextarea.addEventListener('keyup', updateRemainingChars);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var contentTextarea = document.getElementById('editJobTitle');
+    var remainingCharsSpan = document.getElementById('remainingTitleChars');
+
+    function updateRemainingChars() {
+        var remaining = 100 - contentTextarea.value.length;
+        remainingCharsSpan.textContent = remaining + ' characters remaining';
+    }
+
+    updateRemainingChars();
+
+    contentTextarea.addEventListener('keyup', updateRemainingChars);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    var contentTextarea = document.getElementById('editJobDescription');
+    var remainingCharsSpan = document.getElementById('remainingDescriptionChars');
+
+    function updateRemainingChars() {
+        var remaining = 800 - contentTextarea.value.length;
+        remainingCharsSpan.textContent = remaining + ' characters remaining';
+    }
+
+    updateRemainingChars();
+
+    contentTextarea.addEventListener('keyup', updateRemainingChars);
+});
