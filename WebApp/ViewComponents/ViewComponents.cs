265546c0d13f968f137d8291eff7c765d47ee13e
artifecx <kebeniga@gmail.com>
2024-10-08 11:18:26 +0800

    Merge styling changes from Tiu-Frontend

---
+++ WebApp/ViewComponents/ViewComponents.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.ServiceModels;
+
+namespace WebApp.ViewComponents
+{
+    public class SearchInputViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string searchValue, string placeholder = null)
+        {
+            ViewData["SearchValue"] = searchValue;
+            ViewData["Placeholder"] = placeholder;
+            return View();
+        }
+    }
+}
