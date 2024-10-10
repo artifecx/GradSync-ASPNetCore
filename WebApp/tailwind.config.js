8c3a69bc34c9152ff657705006f7aa71b7eda412
CharlotteTaboada <charlotte.taboada@cit.edu>
2024-10-10 17:45:54 +0800

    added Dashboard and modified UI for Admin User Management

--- WebApp/tailwind.config.js
+++ WebApp/tailwind.config.js
@@ -1,21 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-
-
     content: [
         './Pages/**/*.cshtml',
         './Views/**/*.cshtml',
         './wwwroot/**/*.html'
     ],
-  theme: {
-      extend: {
-          backgroundImage: {'login-bg': "url('/img/loginBG.png')",
-              fontFamily: {
-                  sans: ['Inter', 'sans-serif'],
-              },
-          },
-      },
-  },
-  plugins: [],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#7A1515',  //  primary color
+                hover: '#991b1b',     //  hover color
+            },
+            backgroundImage: {
+                'login-bg': "url('/img/loginBG.png')",
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
 }
-
