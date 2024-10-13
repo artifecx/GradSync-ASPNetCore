e91197113c7b0eef74865c3223335ccd65f5f49a
artifecx <kebeniga@gmail.com>
2024-10-13 22:52:49 +0800

    Add email services and rate limiter

---
+++ Services/Interfaces/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using Data.Models;
+using Services.ServiceModels;
+using System.Threading.Tasks;
+using static Resources.Constants.Enums;
+
+namespace Services.Interfaces
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(string toEmail, string subject, string body);
+    }
+}
