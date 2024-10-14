523c4c4fb9af8510e10e8f51ca9145963ba279b6
artifecx <kebeniga@gmail.com>
2024-10-15 04:28:30 +0800

    Implement email queueing system

--- Services/ServiceModels/EmailSettings.cs
+++ Services/ServiceModels/EmailSettings.cs
@@ -1,9 +1,4 @@
-﻿using Data.Models;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-
-namespace Services.ServiceModels
+﻿namespace Services.ServiceModels
 {
     public class EmailSettings
     {
