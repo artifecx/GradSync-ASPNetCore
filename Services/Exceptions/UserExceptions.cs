8f16a58cc49fc5e06a027da5e461a39d430fbdf7
artifecx <kebeniga@gmail.com>
2024-10-22 15:04:48 +0800

    Remove unecessary files, update solution/projects

---
+++ Services/Exceptions/UserExceptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Exceptions
+{
+    public class UserExceptions
+    {
+        public class UserException : Exception
+        {
+            public string Id { get; }
+            public UserException(string message) : base(message) { }
+            public UserException(string message, string id) : base(message)
+            {
+                Id = id;
+            }
+        }
+
+        public class UserNotVerifiedException : Exception
+        {
+            public string Id { get; }
+            public UserNotVerifiedException(string message) : base(message) { }
+            public UserNotVerifiedException(string message, string id) : base(message)
+            {
+                Id = id;
+            }
+        }
+    }
+}
--- WebApp/wwwroot/js/chart.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WebApp.wwwroot.js
-{
-    public class chart
-    {
-    }
-}
